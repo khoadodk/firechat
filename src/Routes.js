5101e497072cdedb8fb77213640b968b016e5794
Khoa Do <khoado.dk@gmail.com>
2020-04-29 12:24:41 -0400

    Wired app to redux and added spinner component

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import firebase from "./firebase";
+import { setUser } from "./actions";
 
 import App from "./components/App";
+import Spinner from "./components/Spinner";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import NotFound from "./components/NotFound";
 
-const Routes = () => {
-  return (
-    <Router>
+class Routes extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // console.log("USER from FIREBASE", user);
+        // console.log("ISLOADING", this.props.isLoading);
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route component={NotFound} />
       </Switch>
-    </Router>
-  );
-};
+    );
+  }
+}
 
-export default Routes;
+// Pass the loading from global state to props of this component
+const mapStateToProps = (state) => {
+  return { isLoading: state.user.isLoading };
+};
+export default withRouter(connect(mapStateToProps, { setUser })(Routes));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import Routes from "./Routes";
 import "semantic-ui-css/semantic.min.css";
+import rootReducer from "./reducers";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Routes />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
